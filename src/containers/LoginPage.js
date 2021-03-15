6143fcaccace4a63dea9c1499730edaa2e6e0f70
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-14 23:32:13 -0700

    Add and fix some css rules for login page. Form is centered.

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -3,17 +3,21 @@ import React from 'react'
 export default function LoginPage() {
     return (
         <div className='loginPageMain'>
-             <div className='loginPageMainOne'>
-                <h3>Login Form</h3>
-                <p>Login to access your Directory</p>
-             </div>
-             <div className='loginPageMainTwo'>
-                <input placeholder='email@example.com'/>
-                <input placeholder='password' />
-             </div>
-            <div className='loginPageMainThree'>
-                <button>Login</button>
-                <p>Don't have an account. <a href='#'>Sign up</a>.</p>
+            <div className='loginPageChild'>
+                <div className='loginPageMainOne'>
+                    <h3>Login Form</h3>
+                    <p>Login to access your Directory</p>
+                </div>
+                <div className='loginPageMainTwo'>
+                    <input placeholder='email@example.com'/>
+                    <input placeholder='password' />
+                </div>
+                <div className='loginPageMainThree'>
+                    <span>
+                        <button>Login</button>
+                    </span>
+                    <p>Don't have an account. <a href='#'>Sign up</a>.</p>
+                </div>
             </div>
         </div>
     )
