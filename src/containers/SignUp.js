414703c0f9507bb0d6c5dace03010cd5ee3e3f40
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-16 00:43:00 -0700

    Create contact section and contact info.

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -4,26 +4,26 @@ export default function SignUp() {
     return (
         <div>
             <div className='pageMain'>
-            <div className='pageChild'>
-                <div className='pageMainOne'>
-                    <h3>Register Form</h3>
-                    <p className='existUser' >This e-mail already registered.</p>
-                </div>
-                <div className='pageMainTwo'>
-                    <input placeholder='name'/>
-                    <input placeholder='lastname' />
-                </div>
-                <div className='pageMainTwo'>
-                    <input placeholder='email@example.com'/>
-                    <input placeholder='password' />
-                </div>
-                <div className='pageMainThree'>
-                    <span>
-                        <button>Submit</button>
-                    </span>
+                <div className='pageChild'>
+                    <div className='pageMainOne'>
+                        <h3>Register Form</h3>
+                        <p className='existUser' >This e-mail already registered.</p>
+                    </div>
+                    <div className='pageMainTwo'>
+                        <input placeholder='name'/>
+                        <input placeholder='lastname' />
+                    </div>
+                    <div className='pageMainTwo'>
+                        <input placeholder='email@example.com'/>
+                        <input placeholder='password' />
+                    </div>
+                    <div className='pageMainThree'>
+                        <span>
+                            <button>Submit</button>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -13,6 +13,63 @@ export default function UserPage() {
                     <li className='userPageLi'><a href='#'>Logout</a></li>
                 </ul>
             </div>
+            <div className='pageMainUser'>
+                <div className='pageChild'>
+                    <div className='pageMainOne'>
+                        <h3>This is your directory page.</h3>
+                        <p>You can create your contact list click to 'Create' button.</p>
+                    </div>
+                    <div className='pageMainThree'>
+                        <span>
+                            <button>Create</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className='mainContact'>
+                <div className='pageMainContact'>
+                    <div className='pageChildContact'>
+                        <div className='pageMainOneContact'>
+                            <div className='phoneIcon'>
+                                <i className="far fa-address-card fa-5x" ></i>
+                            </div>
+                            <div className='infoArea'>
+                                <h4>Kaan Catik</h4>
+                                <p>Web Developer</p>
+                                <p>+1 (123)456 7890</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='pageMainContact'>
+                    <div className='pageChildContact'>
+                        <div className='pageMainOneContact'>
+                            <div className='phoneIcon'>
+                                <i className="far fa-address-card fa-5x" ></i>
+                            </div>
+                            <div className='infoArea'>
+                                <h4>Kaan Catik</h4>
+                                <p>Web Developer</p>
+                                <p>+1 (123)456 7890</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='pageMainContact'>
+                    <div className='pageChildContact'>
+                        <div className='pageMainOneContact'>
+                            <div className='phoneIcon'>
+                                <i className="far fa-address-card fa-5x" ></i>
+                            </div>
+                            <div className='infoArea'>
+                                <h4>Kaan Catik</h4>
+                                <p>Web Developer</p>
+                                <p>+1 (123)456 7890</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
