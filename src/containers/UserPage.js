e2017bbb0d6c13280ff51655e38f5c1acb308d07
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-15 22:43:29 -0700

    Some css rules added.

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -4,15 +4,15 @@ import '../UserPage.css'
 export default function UserPage() {
     return (
         <div className='userPageMain'>
-            <navbar className='userPageNav'>
+            <div className='userPageNav'>
                 <span className='userPageSpanOne'>
                     <h3 className='userPageh3One'>Welcome Kaan Catik</h3>
                 </span>
-                <span className='userPageSpanTwo'>
-                    <h3 className='userPageh3Two'>Account</h3>
-                    <h3 className='userPageh3Two'>Logout</h3>
-                </span>
-            </navbar>
+                <ul className='userPageUl'>
+                    <li className='userPageLi'><a href='#'>Account</a></li>
+                    <li className='userPageLi'><a href='#'>Logout</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
