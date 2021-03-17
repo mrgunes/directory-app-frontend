a8c9c0a4c8e473a5af6fa77d0da0849d0362414f
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-16 21:29:19 -0700

    Contact form created and css rules are applied.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,17 @@ import '../App.css';
 import LoginPage from './LoginPage'
 import SignUp from './SignUp';
 import UserCreated from './UserCreated';
-import UserPage from  './UserPage'
+import UserPage from  './UserPage';
+import ContactPage from './ContactPage'
 
 function App() {
   return (
     <div className="main">
       {/* <LoginPage/> */}
-      <SignUp/>
+      {/* <SignUp/> */}
       {/* <UserCreated/> */}
       {/* <UserPage/> */}
+      <ContactPage/>
     </div>
   );
 }
---
+++ src/containers/ContactPage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function ContactPage() {
+    return (
+        <div className='userPageMain'>
+            <div className='contactPageNav'>
+                <span className='contactPageSpanOne'>
+                    <h3 className='contactPageh3One'>Welcome Kaan Catik</h3>
+                </span>
+                <ul className='contactPageUl'>
+                    <li className='contactPageLi'><a href='#'>Account</a></li>
+                    <li className='contactPageLi'><a href='#'>Logout</a></li>
+                </ul>
+            </div>
+            <div className='pageMainContactArea'>
+                <div className='pageChild'>
+                    <div className='pageMainOne'>
+                        <h3>Contact Form</h3>
+                    </div>
+                    <div className='pageMainContactAreaTwo'>
+                        <div className='pageMainContactAreaTwoDivOne'>
+                            <input placeholder='Name, Last Name'/>
+                            <input placeholder='E-mail'/>
+                            <input placeholder='Phone' />
+                            <textarea className='pageContactTextAreaOne' placeholder='Address'></textarea>
+                            <input placeholder='Website' />
+                        </div>
+                        <div className='pageMainContactAreaTwoDivTwo'>
+                            <input placeholder='@Social' />
+                            <input placeholder='Company' />
+                            <input placeholder='Job' />
+                            <textarea className='pageContactTextAreaTwo' placeholder='Note'></textarea>
+                        </div>
+                    </div>
+                    <div className='pageContactButton'>
+                        <span className='button'>
+                            <button>Save</button>
+                            <button>Cancel</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
