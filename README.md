5910cd56f2afca8f1d6e33e9ea5bce7ea634f211
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-18 14:27:55 -0700

    Updated README.md.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Directory App
 
-You can create, update or delete your directory with Directory App. Directory App is included name, last name, phone, address and note areas.
+You can create, update or delete your directory with Directory App. Directory App is included name, last name, phone, address, social, company, job and note areas.
--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,22 +1,55 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 
 export default function LoginPage() {
+    let history = useHistory();
+    let [email, setEmail]=useState('');
+    let [password, setPassword]=useState('');
+    let [findUser, setFindUser]=useState('notFound');
+    let [checkUser, setCheckUser]=useState('wrongUser')
+
+    let handleEmail=(e)=>{
+        setEmail(e.target.value)
+    }
+
+    let handlePassword=(e)=>{
+        setPassword(e.target.value)
+    }
+
+    console.log(email, password);
+
+    let onLogin=()=>{
+        if (email!=='test@test.com'){
+            if(checkUser==='wrongUserP'){
+                setCheckUser('wrongUser')
+                setFindUser('notFoundP')
+            }
+            setFindUser('notFoundP')
+        } else if (email==='test@test.com' && password!=='12345'){
+            if(findUser==='notFoundP'){
+                setFindUser('notFound')
+                setCheckUser('wrongUserP')
+            }
+            setCheckUser('wrongUserP')
+        } else if (email==='test@test.com' && password==='12345')
+            history.push('/userpage')   
+    }
+
     return (
         <div className='pageMain'>
             <div className='pageChild'>
                 <div className='pageMainOne'>
                     <h3>Login Form</h3>
                     <p>Login to access your Directory</p>
-                    <p className='notFound' >User was not found.</p>
-                    <p className='wrongUser' >Wrong e-mail or password.</p>
+                    <p className={findUser} >User was not found.</p>
+                    <p className={checkUser} >Email or password doesn't match.</p>
                 </div>
                 <div className='pageMainTwo'>
-                    <input placeholder='email@example.com'/>
-                    <input placeholder='password' />
+                    <input placeholder='email@example.com' onChange={handleEmail}/>
+                    <input placeholder='password' onChange={handlePassword}/>
                 </div>
                 <div className='pageMainThree'>
-                    <Link to='/userpage'><button>Login</button></Link>
+                    <button onClick={onLogin}>Login</button>
                     <p>Don't have an account? <Link to='/signup'>Sign Up</Link>.</p>
                 </div>
             </div>
